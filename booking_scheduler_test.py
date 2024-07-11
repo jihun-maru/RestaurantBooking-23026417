240732341a4a5cee25cbe4aac348fa204bd45ae6
jihun75.kim <gnswlrla0218@naver.com>
2024-07-11 11:33:08 +0900

    feature testcode step6-2

--- booking_scheduler_test.py
+++ booking_scheduler_test.py
@@ -2,6 +2,7 @@ import unittest
 from datetime import datetime, timedelta
 
 from booking_scheduler import BookingScheduler
+from communication_test import TestableSmsSender
 from schedule import Customer, Schedule
 
 UNDER_CAPACITY = 1
@@ -55,8 +56,13 @@ class BookingSchedulerTest(unittest.TestCase):
         self.assertTrue(self.booking_scheduler.has_schedule(schedule))
 
     def test_예약완료시_SMS는_무조건_발송(self):
-        pass
+        testable_sms_sender = TestableSmsSender()
+        schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+        self.booking_scheduler.set_sms_sender(testable_sms_sender)
 
+        self.booking_scheduler.add_schedule(schedule)
+
+        self.assertTrue(testable_sms_sender.is_send_method_is_called())
     def test_이메일이_없는_경우에는_이메일_미발송(self):
         pass
 
